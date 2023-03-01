a318703fe1bcf5a0c1d6f1c9e0a71892377944ce
leetaaan <2011436@dlu.edu.vn>
2023-03-01 16:31:18 +0700

    init commit

---
+++ TipsAndTricks/TatBlog.Data/Contexts/BlogDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Mappings;
+namespace TatBlog.Data.Contextsc
+{
+    internal class BlogDbContext : DbContext
+    {
+        public DbSet<Author> Authors { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server= DESKTOP-CC408KD;Database=TagBlog;Trusted_Connection=True;
+MultipleActiveResultSets=True;TrustServerCertificate=True");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(CategoryMap).Assembly);
+        }
+    }
+}
---
+++ TipsAndTricks/TatBlog.Data/Seeders/DataSeeder.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TatBlog.Core.Entities;
+using TatBlog.Data.Contextsc;
+namespace TatBlog.Data.Seeders
+{
+    public class DataSeeder : IDataSeeder
+    {
+        private readonly BlogDbContext _dbContext;
+
+        public DataSeeder(BlogDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Initialize()
+        {
+            _dbContext.Database.EnsureCreated();
+            if (_dbContext.Posts.Any()) return;
+
+            var authors = AddAuthors();
+            var categories = AddCategories();
+            var tags = AddTags();
+            var posts = AddPosts(authors, categories, tags);
+        }
+        private IList<Author> AddAuthors() { }
+        private IList<Category> AddCategories() { }
+        private IList<Tag> AddTags() { }
+        private IList<Post> AddPosts(
+            IList<Author> authors,
+            IList<Category> categories,
+            IList<Tag> tags)
+        { }
+    }
+
+
+
+}
