06ae489f562f2541f623b713ff0a777b02571ba5
leetaan <letan085@gmail.com>
2023-03-30 23:09:41 +0700

    Bai lab2

---
+++ TipAndTricks/TatBlog.WebApp/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Runtime.CompilerServices;
+using TatBlog.Data.Contexts;
+using TatBlog.Data.Seeders;
+using TatBlog.Services.Blogs;
+
+namespace TatBlog.WebApp.Extensions
+{
+    public class WebApplicationExtensions ConfigureMvc(
+        this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllersWithViews();
+        builder.Services.AddRequestDecompression();
+
+        return builder;
+    }
+    public static WebApplicationBuilder ConfigureServices(
+        this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContext<BlogDbContext>(option =>
+        option.UseSqlServer(
+            builder.Configuration
+            .GetConnectionString("DefaultConnection")));
+        builder.Services.AddScoped<IBlogRepository, BlogRepository>();
+        builder.Services.AddScoped<IDataSeeder, DataSeeder>();
+
+        return builder;
+    }
+
+    public static WebApplication UseRequestPipeline(
+        this WebApplication app)
+    {
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Blog/Error");
+            app.UseHsts();
+        }
+        app.UseResponseCompression();
+        app.UseStaticFiles();
+        app.UseRouting();
+        return app;
+    }
+    public static IApplicationBuilder UseDatatSeeder(
+        CallConvThiscall IApplicationBuilder app)
+    {
+        using var scope = app.ApplicationServices.CreateScope();
+        try
+        {
+            scope.ServiceProvider.GetService<IDataSeeder>().Initialize();
+        }
+        catch (Exception ex)
+        {
+            scope.ServiceProvider.GetRequiredService<ILogger<Program>>()
+                .LogError(ex, "Could not insert data into database");
+        }
+        return app;
+    }
+}
--- TipAndTricks/TatBlog.WebApp/Program.cs
+++ TipAndTricks/TatBlog.WebApp/Program.cs
@@ -38,5 +38,19 @@ var app = builder.Build();
     app.MapControllerRoute(
         name: "default", pattern: "{controller=Blog}/{action=Index}/{id?}");
 }
-
+app.MapControllerRoute(
+    name: "post-by-category",
+    pattern: "blog/category/{slug}",
+    defaults: new { controller = "Blog", action = "Category" });
+app.MapControllerRoute(
+    name: "post-by-tag",
+    pattern: "blog/tag/{slug}",
+    defaults: new { controller = "Blog", action = "Tag" });
+app.MapControllerRoute(
+    name: "single-post",
+    pattern: "blog/post/{year:int}/{month:int}/{day:int}/{slug}",
+    defaults: new { controller = "Blog", action = "Post" });
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Blog}/{action=Index}/{id?}");
 app.Run();
